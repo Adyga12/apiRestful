022b247f0c4a0be8cfaa1fc5a1b78fc148e88b89
Gabriela Gamboa <adrianagg1221@gmail.com>
2022-11-25 01:40:48 -0600

    second commit

--- server.js
+++ server.js
@@ -3,29 +3,33 @@ const express = require('express');
 const Container = require('./contenedor')
 const servidor = express();
 const PORT = 8000;
-let products = null;
 
 servidor.use(express.json());
 
-function onInit() {
-    products = getAllProducts();
-    console.log("Productos cargados: ", products);
-}
 
 servidor.get("/api/products", (peticion, respuesta) => {
-    respuesta.send(products);
+    const container = new Container();
+    const file = "./products.txt";
+    const allProductsArray = container.read(file);
+    respuesta.json(allProductsArray);
 });
+
 
 servidor.get("/api/product/:id", (peticion, respuesta) => {
     const { id } = peticion.params
-    respuesta.json({ buscada: products[parseInt(id) - 1] })
+    const container = new Container();
+    const file = "./products.txt";
+    const allProductsArray = container.read(file);
+    respuesta.json({ buscada: allProductsArray[parseInt(id) - 1] })
 });
 
 servidor.post('/api/products', (peticion, respuesta) => {
-    const { product } = peticion.body
-    products.push(product)
-    respuesta.json({ agregado: product, posicion: products.length })
-})
+    const { nombre, cantidad, precio } = peticion.body;
+    const nuevoProducto = { nombre, cantidad, precio };
+    const products = new Container();
+    const product = products.save(nuevoProducto);
+    respuesta.json({ agregada: nuevoProducto, posicion: products.length })
+});
 
 servidor.put('/api/products/:id', (peticion, respuesta) => {
     const { product } = peticion.body
@@ -33,24 +37,16 @@ servidor.put('/api/products/:id', (peticion, respuesta) => {
     const productAnt = products[parseInt(id) - 1]
     products[parseInt(id) - 1] = product
     respuesta.json({ actualizada: product, anterior: productAnt })
-})
+});
 
 
 servidor.delete('/api/products/:id', (peticion, respuesta) => {
     const { id } = peticion.params
     const [product] = products.splice(parseInt(id) - 1, 1)
     respuesta.json({ borrada: product })
-})
-
-function getAllProducts() {
-    const container = new Container();
-    const file = "./products.txt";
-    const allProductsArray = container.read(file);
-    return allProductsArray;
-}
+});
 
 
-onInit();
 
 servidor.listen(8000);
 console.log("Servidor corriendo en el puerto " + PORT);
