95bdec091dbf89312f9385922afced9e6ee5cb1c
Gabriela Gamboa <adrianagg1221@gmail.com>
2022-12-05 01:57:49 -0600

    first commit

--- server.js
+++ server.js
@@ -1,59 +1,17 @@
-const http = require('http');
-const express = require('express');
-const Container = require('./contenedor');
-const server = express();
+const express = require("express");
+const handlenbars = require("express-handlebars");
+const productsRouter = require("./routes/products.js");
 
+const app = express();
 
-server.set("port", process.env.PORT || 8000);
-const PORT = server.get("port");
-
-server.use(express.json());
-const container = new Container();
-const file = "./products.txt";
-
-server.get("/api/products", (req, res) => {
-    const allProductsArray = container.read(file);
-    res.json(allProductsArray);
+const server = app.listen(8080, () => {
+    console.log("listening on port 8080");
 });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.use("/", productsRouter);
 
-server.get("/api/product/:id", (req, res) => {
-    const { id } = req.params
-    const allProductsArray = container.read(file);
-    res.json({ buscada: allProductsArray[parseInt(id) - 1] })
-});
-
-server.post('/api/products', (req, res) => {
-    const { nombre, cantidad, precio } = req.body;
-    const products = container.getAll("./products.txt");
-    const newProduct = { id: products.length, nombre, cantidad, precio, };
-    container.addNewProduct(newProduct);
-    res.json({ agregada: newProduct.id });
-});
-
-
-server.put('/api/products/:id', (req, res) => {
-    const product = req.body;
-    const { id } = req.params;
-    const oldProduct = container.updateProductById(id, product);
-    res.json({ productUpdated: product, oldProduct });
-});
-
-
-server.delete('/api/products/:id', (req, res) => {
-    const { id } = req.params
-    if (!container.getById(id, "./products.txt")) {
-        res.json({
-            error: "This id doesn't exist"
-        })
-    } else {
-        container.deleteByIdByMe(id);
-        res.json({
-            successfully: "product deleted"
-        });
-    }
-});
-
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT + `\nVisit: http://localhost:${PORT}`);
-});
+app.engine("handlebars", handlenbars.engine());
+app.set("views", "./views");
+app.set("view engine", "handlebars");
